25515b62b88278512a388d1db66d6d0fb4404219
Christian Perez <perezchristian1012@gmail.com>
2019-12-28 01:16:31 +0000

    created users table in db folder

---
+++ db/migrate/20191228011212_create_users_table.rb
@@ -0,0 +1,11 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.text :bio
+      t.string :image_url
+    end
+  end
+end
