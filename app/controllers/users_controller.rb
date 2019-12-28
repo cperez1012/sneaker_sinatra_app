e6fbddf3a7f16a716cfdfb64dda8f93f40e3a21e
Christian Perez <perezchristian1012@gmail.com>
2019-12-28 03:13:47 +0000

    Added /login route in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
+  #render a login form
   get "/login" do
-    "login route!"
+    erb :'users/login'
+  end
+
+  #receive data from the login form (params)
+  post "/login" do
+    #find the user
+    user = User.find_by(email: params[:email])
+    #authenticate the user
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:message] = "Welcome back #{user.name}"
+    #log them in
+    #redirect to user's profile (user's show)
+      redirect "/users/#{user.id}"
+    else
+      flash[:error] = "Your credentials were invalid. Try again!"
+      redirect '/login'
+    end
   end
 
 end
