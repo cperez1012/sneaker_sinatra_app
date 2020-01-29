abe2705ef03db7deecc8acc24f799b5b5949f518
Christian Perez <perezchristian1012@gmail.com>
2020-01-29 17:10:48 +0000

    updated sneakers controller

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -15,12 +15,21 @@ class SneakersController < ApplicationController
     end
   end
 
-  get '/sneakers/:user_id' do
-    #id from params is current user id
-    # @sneaker = Sneaker.find(params[:user_id])
-    # binding.pry
-    @user = User.find(params[:user_id])
-    erb :"sneakers/show"
+  post '/sneakers/new' do
+    if logged_in? && params[:name] != "" && params[:image_url] != "" && params[:description] != "" && params[:category] != ""
+      @new_sneaker = Sneaker.create(params)
+      @sneaker_slug = "#{@new_sneaker.name} #{@new_sneaker.image_url} #{@new_sneaker.description} #{@new_sneaker.category}".downcase.gsub('','+')
+      @new_sneaker.user_id = current_user.id
+      @new_sneaker.save
+      redirect to '/users/show'
+    else
+      redirect to '/users/show'
+    end
+  end
+
+  get '/sneakers/:id' do
+    @sneakers = Sneaker.find(params[:id])
+    erb :'/sneakers/show'
   end
 
   get '/sneakers/:id/edit' do
@@ -33,30 +42,44 @@ class SneakersController < ApplicationController
     end
   end
 
-  post '/sneakers' do
-    # binding.pry
-    @sneaker = Sneaker.new(params[:sneaker])
-    # binding.pry
-    @sneaker.user_id = current_user.id
-    if @sneaker.save
-      flash[:message] = "Sneaker saved successfully!"
-      redirect to "/sneakers/#{@sneaker.id}"
-    else
-      flash[:error] = "Sneaker Creation Failed"
-      redirect '/sneakers/new'
-    end
-  end
-
-  get '/sneakers/:slug/edit' do
-   @sneakers = Sneaker.all
-   @sneaker = Song.find_by_slug(params[:slug])
-   erb :'sneakers/edit'
- end
-
   patch '/sneakers/:id' do
     @sneaker = Sneaker.find(params[:id])
     @sneaker.update(name: params[:name], category: params[:category], description: params[:description], image_url: params[:image_url])
     redirect to "/sneakers/#{@sneakers.id}"
   end
+
+  get '/sneakers/:id/delete' do
+    @sneaker = Sneaker.find(params[:id])
+    @sneaker.delete
+    redirect to '/sneakers'
+  end
+
+ #  get '/sneakers/:user_id' do
+ #    #id from params is current user id
+ #    # @sneaker = Sneaker.find(params[:user_id])
+ #    # binding.pry
+ #    @user = User.find(params[:user_id])
+ #    erb :"sneakers/show"
+ #  end
+ #
+ #  post '/sneakers' do
+ #    # binding.pry
+ #    @sneaker = Sneaker.new(params[:sneaker])
+ #    # binding.pry
+ #    @sneaker.user_id = current_user.id
+ #    if @sneaker.save
+ #      flash[:message] = "Sneaker saved successfully!"
+ #      redirect to "/sneakers/#{@sneaker.id}"
+ #    else
+ #      flash[:error] = "Sneaker Creation Failed"
+ #      redirect '/sneakers/new'
+ #    end
+ #  end
+ #
+ #  get '/sneakers/:slug/edit' do
+ #   @sneakers = Sneaker.all
+ #   @sneaker = Song.find_by_slug(params[:slug])
+ #   erb :'sneakers/edit'
+ # end
 
 end
