e429852ec8c0564f710ac180f8a0ec27613e8f8d
Christian Perez <perezchristian1012@gmail.com>
2020-01-29 18:35:45 +0000

    updated sneakers controller's edit and index controls and views

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -28,24 +28,19 @@ class SneakersController < ApplicationController
   end
 
   get '/sneakers/:id' do
-    @sneakers = Sneaker.find(params[:id])
+    @sneaker = Sneaker.find(params[:id])
     erb :'/sneakers/show'
   end
 
   get '/sneakers/:id/edit' do
     @sneaker = Sneaker.find(params[:id])
-    if authorized_to_edit?(@sneaker)
-      erb :'sneakers/edit'
-    else
-      flash[:error] = "Not authorized to edit!"
-      redirect to '/sneakers'
-    end
+    erb :'/sneakers/edit'
   end
 
   patch '/sneakers/:id' do
     @sneaker = Sneaker.find(params[:id])
     @sneaker.update(name: params[:name], category: params[:category], description: params[:description], image_url: params[:image_url])
-    redirect to "/sneakers/#{@sneakers.id}"
+    redirect to "/sneakers/#{@sneaker.id}"
   end
 
   get '/sneakers/:id/delete' do
@@ -53,32 +48,4 @@ class SneakersController < ApplicationController
     @sneaker.delete
     redirect to '/sneakers'
   end
-
- #  get '/sneakers/:user_id' do
- #    #id from params is current user id
- #    # @sneaker = Sneaker.find(params[:user_id])
- #    # binding.pry
- #    @user = User.find(params[:user_id])
- #    erb :"sneakers/show"
- #  end
- #
- #  post '/sneakers' do
- #    # binding.pry
- #    @sneaker = Sneaker.new(params[:sneaker])
- #    # binding.pry
- #    @sneaker.user_id = current_user.id
- #    if @sneaker.save
- #      flash[:message] = "Sneaker saved successfully!"
- #      redirect to "/sneakers/#{@sneaker.id}"
- #    else
- #      flash[:error] = "Sneaker Creation Failed"
- #      redirect '/sneakers/new'
- #    end
- #  end
- #
- #  get '/sneakers/:slug/edit' do
- #   @sneakers = Sneaker.all
- #   @sneaker = Song.find_by_slug(params[:slug])
- #   erb :'sneakers/edit'
- # end
 end
