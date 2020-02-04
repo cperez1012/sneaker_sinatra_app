5c6912caed6af340c6cb637820a41eb56a0e351b
Christian Perez <perezchristian1012@gmail.com>
2020-02-04 13:33:54 +0000

    added front end changes to background and changed headings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,4 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, presence: {message: "That username is already taken, please use another username."}
   validates_uniqueness_of :email, presence: {message: "That email is already associated to another account. Please use another email."}
 
-# include Slugifiable::InstanceMethods
-# extend Slugifiable::ClassMethods
-
 end
